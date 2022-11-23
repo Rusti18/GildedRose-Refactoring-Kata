3f83ac9d0c8e5c170da4cd3c0f10aab7a749acfe
Mihai Rustiuc <mihai.rustiuc@protonmail.com>
2022-11-23 18:17:53 +0200

    Make initial test pass

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,10 +2,10 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
+    func test_foo() throws {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+        XCTAssertEqual(app.items[0].name, "foo")
     }
 }
