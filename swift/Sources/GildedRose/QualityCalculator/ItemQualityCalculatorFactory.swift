6e4402a40272aab0cefc2e670f0c53cac31a2077
Mihai Rustiuc <mihai.rustiuc@protonmail.com>
2022-11-24 13:03:20 +0200

    Create `ItemQualityCalculatorFactory` that creates an `ItemQualityCalculator` based on item name

---
+++ swift/Sources/GildedRose/QualityCalculator/ItemQualityCalculatorFactory.swift
@@ -0,0 +1,23 @@
+//
+//  ItemQualityCalculatorFactory.swift
+//  
+//
+//  Created by Mihai Andrei Rustiuc on 24.11.2022.
+//
+
+import Foundation
+
+public enum ItemQualityCalculatorFactory {
+    public static func createItemQualityCalculator(item: Item) -> ItemQualityCalculator {
+        let qualityCalculator: ItemQualityCalculator
+        
+        switch item.name {
+        case "Aged Brie": qualityCalculator = AgedBrieItemQualityCalculator(item: item)
+        case "Sulfuras, Hand of Ragnaros": qualityCalculator = SulfurasItemQualityCalculator(item: item)
+        case "Backstage passes to a TAFKAL80ETC concert": qualityCalculator = BackstagePassesItemCalculator(item: item)
+        default: qualityCalculator = ItemQualityCalculator(item: item)
+        }
+        
+        return qualityCalculator
+    }
+}
