7f18727b5cdf82eeb419b0abce5554ab9cd65f9e
Mihai Rustiuc <mihai.rustiuc@protonmail.com>
2022-11-24 11:03:54 +0200

    Extract helper function that works directly on item, thus avoiding index based access that is error-prone

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -6,52 +6,54 @@ public class GildedRose {
     }
 
     public func updateQuality() {
-        for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie", items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
+        items.forEach { updateQuality(for: $0) }
+    }
+    
+    private func updateQuality(for item: Item) {
+        if item.name != "Aged Brie", item.name != "Backstage passes to a TAFKAL80ETC concert" {
+            if item.quality > 0 {
+                if item.name != "Sulfuras, Hand of Ragnaros" {
+                    item.quality = item.quality - 1
                 }
-            } else {
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
+            }
+        } else {
+            if item.quality < 50 {
+                item.quality = item.quality + 1
 
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.sellIn < 11 {
+                        if item.quality < 50 {
+                            item.quality = item.quality + 1
                         }
+                    }
 
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    if item.sellIn < 6 {
+                        if item.quality < 50 {
+                            item.quality = item.quality + 1
                         }
                     }
                 }
             }
+        }
 
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+        if item.name != "Sulfuras, Hand of Ragnaros" {
+            item.sellIn = item.sellIn - 1
+        }
 
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
+        if item.sellIn < 0 {
+            if item.name != "Aged Brie" {
+                if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.quality > 0 {
+                        if item.name != "Sulfuras, Hand of Ragnaros" {
+                            item.quality = item.quality - 1
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
                 }
             }
         }
